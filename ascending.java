bd514d10117c349ae7450fe56de05ef8ca55edd3
Vedant Manve <vedantmanve2210@gmail.com>
2026-03-06 15:51:30 +0530

    first commit

---
+++ ascending.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ascending {
+        public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter size of array:");
+        int n = sc.nextInt();
+        
+        int arr[] = new int[n];
+        
+        System.out.println("Enter array elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        
+        System.out.println("Array in Ascending Order:");
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        
+    }
+
+}
